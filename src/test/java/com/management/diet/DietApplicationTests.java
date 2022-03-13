1d0efdd935045e5c2df2bd61886775a047ff6183
jeayoung <k01066624566@gmail.com>
2022-03-14 08:26:00 +0900

    first commit

---
+++ src/test/java/com/management/diet/DietApplicationTests.java
@@ -0,0 +1,13 @@
+package com.management.diet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
