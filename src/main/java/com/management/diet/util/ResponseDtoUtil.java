3200675d17d6a325a5f93a173f798ea32685f767
jeayoung <k01066624566@gmail.com>
2022-06-10 08:10:16 +0900

    update ResponseDtoUtil

--- src/main/java/com/management/diet/util/ResponseDtoUtil.java
+++ src/main/java/com/management/diet/util/ResponseDtoUtil.java
@@ -1,68 +1,29 @@
 package com.management.diet.util;
 
-import com.management.diet.domain.Member;
-import com.management.diet.domain.Posting;
-import com.management.diet.dto.response.MemberLoginResponseDto;
-import com.management.diet.dto.response.MemberResponseDto;
-import com.management.diet.dto.response.PostingResponseDto;
-import com.management.diet.dto.response.ResponseDto;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
 
 @Component
 public class ResponseDtoUtil {
+    private static ModelMapper mapper = new ModelMapper();
 
-    public <T> ResponseDto oneToResponse(ResponseDto responseDto, T data){
-        return isSupport(responseDto, data);
+    static {
+        mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
     }
 
-    private <T>ResponseDto isSupport(ResponseDto responseDto, T data) {
-        if(responseDto instanceof PostingResponseDto){
-            PostingResponseDto postingResponseDto = (PostingResponseDto) responseDto;
-            if(data instanceof Posting){
-                Posting posting = (Posting) data;
-                postingResponseDto = PostingResponseDto.builder()
-                        .postIdx(posting.getPostingIdx())
-                        .title(posting.getTitle())
-                        .content(posting.getContent())
-                        .member(posting.getMember())
-                        .comments(posting.getComments())
-                        .date(posting.getDate())
-                        .fix(posting.getFix())
-                        .goods(posting.getGoods())
-                        .build();
-                return postingResponseDto;
-            }
-            else{
-                throw new RuntimeException();
-            }
-        }
-        else if(responseDto instanceof MemberResponseDto){
-            MemberResponseDto memberResponseDto = (MemberResponseDto) responseDto;
-            if(data instanceof Member){
-                Member member = (Member) data;
-                memberResponseDto = MemberResponseDto.builder()
-                        .memberIdx(member.getMember_idx())
-                        .name(member.getName())
-                        .postings(member.getPostings())
-                        .profile(member.getProfile())
-                        .theme(member.getTheme())
-                        .build();
-                return memberResponseDto;
-            }
-            else{
-                throw new RuntimeException();
-            }
+    public static <D,T> D mapping(T entity, Class<D> dto){
+        return mapper.map(entity, dto);
+    }
 
-        }
-        else if(responseDto instanceof MemberLoginResponseDto){
-            responseDto = (MemberLoginResponseDto) responseDto;
-        }
-        else{
-            throw new RuntimeException();
-        }
-        return null;
+    public static <D, T> List<D> mapAll(final List<T> entityList, Class<D> dto) {
+        return entityList.stream()
+                .map(entity -> mapper.map(entity, dto))
+                .collect(Collectors.toList());
     }
 }
